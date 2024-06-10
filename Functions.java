01739e76a1819383869132dd63596361982600f7
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-06-10 16:22:09 +0530

    changes

---
+++ Functions.java
@@ -0,0 +1,14 @@
+public class Functions{
+    public static void printMyName(String name) {
+        System.out.println("My name is " + name);
+        return;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        printMyName(name); //call kiya function ko
+
+    }
+}
