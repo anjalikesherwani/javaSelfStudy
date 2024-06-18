f644fb0082f71aae9373bfa66c2052388a5ab95a
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-06-18 16:01:54 +0530

    arrayloop

--- Array/arrayJava.java
+++ Array/arrayJava.java
@@ -13,8 +13,37 @@ public class arrayJava {
         System.out.println(myNum);
         System.out.println(myNum[1]);
 
-        
+        // String[] car = {"Volvo", "BMW", "Ford", "Mazda"};
+        // for (int i = 0; i < car.length; i++) {
+        //   System.out.println(car[i]);
+        // }
 
+
+        // String[] car = {"Volvo", "BMW", "Ford", "Mazda"};
+        // for (String i : car) {
+        //   System.out.println(i);
+        // }
+
+
+        int ages[] = {20,23,34,12,24,35,43,12,21};
+        float avg, sum = 0;
+
+        // get the length of the array
+        int length = ages.length;
+
+        // loop through the elements of the array
+        for(int age : ages){
+            sum += age;
+        }
         
+        // calculate the average by dividing the sum byb the length
+        avg = sum/length;
+        
+        // print the average
+        for (int i : ages) {
+        System.out.println("Age : "+ i);}
+        System.out.println("Sum : "+ sum);
+        System.out.println("length : " + length);
+        System.out.println("The Average age is : "+ avg);
     }
 }
