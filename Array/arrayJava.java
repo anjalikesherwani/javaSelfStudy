1ae37a9509a4df2fb4b1aa91bbb624408ce42ac1
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-06-18 15:34:32 +0530

    array1

--- Array/arrayJava.java
+++ Array/arrayJava.java
@@ -1,5 +1,20 @@
 public class arrayJava {
     public static void main(String[] args) {
+        String[] cars= {"Volvo","BMW","Ford","Mazda"};
+        System.out.println(cars);
+        System.out.println(cars[2]);
+
+        cars[0] = "OPel";
+        System.out.println(cars[0]);
+
+        System.out.println(cars.length);
+
+        int[] myNum = {10,20,34,45}; 
+        System.out.println(myNum);
+        System.out.println(myNum[1]);
+
+        
+
         
     }
 }
